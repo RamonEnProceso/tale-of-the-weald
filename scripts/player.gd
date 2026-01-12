cff3ca318e26f9e59d9905fbb7a44d62b56d5466
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-01-12 03:35:06 -0300

    agregar sombra 2D al jugador

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,6 +8,8 @@ class_name Player
 @onready var collectableComponent : CollectableComponent = $CollectableComponent
 @onready var camera : Camera3D = $springPivot3D/Camera3D
 
+@onready var shadow_ray = $ShadowRay
+@onready var shadow_decal : Decal = $ShadowDecal
 
 @export var DEATH_STATE: pState 
 
@@ -21,9 +23,33 @@ func _ready():
 func _unhandled_key_input(event):
 	stateMachine.process_input(event)
 
+func update_shadow_logic():
+	shadow_ray.force_shapecast_update()
+	
+	shadow_ray.force_shapecast_update()
+	
+	if shadow_ray.is_colliding():
+		shadow_decal.visible = true
+		
+		var collision_point = shadow_ray.get_collision_point(0)
+		
+		shadow_decal.global_position.y = collision_point.y + 0.1
+		
+		var distance = global_position.y - collision_point.y
+		shadow_decal.albedo_mix = lerp(1.0, 0.0, clamp(distance / 10.0, 0.0, 1.0))
+		
+		var target_size = lerp(1.5, 0.5, clamp(distance / 10.0, 0.0, 1.0))
+		shadow_decal.size.x = target_size
+		shadow_decal.size.z = target_size
+	else:
+		shadow_decal.visible = false
+
 func _physics_process(delta):
 	stateMachine.process_physics(delta)
 	
+	#Actualiza la sombra del PJ
+	update_shadow_logic()
+
 func _process(delta):
 	stateMachine.process_frame(delta)
 
