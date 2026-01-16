9e221a15fd81dd4d1a7bf4a54b96de7ebb5cf7a8
DanteGPV <dantegpv2004@gmail.com>
2026-01-16 14:10:41 -0300

    Añado Base de spore-attack

--- scripts/attack.gd
+++ scripts/attack.gd
@@ -2,3 +2,8 @@ extends Node
 class_name Attack
 
 @export var ATTACK_DAMAGE : float = 1
+
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	if area.get_parent() is Enemy:
+		area.damage(self)
